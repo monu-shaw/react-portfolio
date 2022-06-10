044aebe5b9c5cd7b0c765048f4225f3c0d13347c
Manish Shaw <manishshaw725@gmail.com>
2022-06-10 19:06:37 +0530

    df

--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -1,12 +1,21 @@
 const { createSlice } = require("@reduxjs/toolkit");
 
-
+const initialState = {
+  cartItem: [],
+}
 const cartSlice = createSlice({
     name: 'cart',
-    initialState: [],
+    initialState ,
     reducers: {
         add(state, action) {
-         state.push(action.payload)
+         
+         if(state.cartItem.find((i)=>i.id === action.payload.id)){
+           console.log('added');
+         }else{
+           let array = action.payload;
+          state.cartItem.push(Object.assign(merge(array, {quantity: 1})));  
+          console.log(state.cartItem)
+         }
         },
         remove(state, action){
           return   state.filter((item)=>item.id !== action.payload);
--- src/store/productSlice.js
+++ src/store/productSlice.js
@@ -1,4 +1,4 @@
-const {createSlice} = require('@reduxjs/toolkit')
+const {createSlice, createAsyncThunk} = require('@reduxjs/toolkit')
 
 
 const STATUS = Object.freeze({
@@ -15,32 +15,49 @@ const productSlice = createSlice({
         status: STATUS.IDLE,
     },
     reducers:{
-    
-        setProduct(state, action){state.data = action.payload}
-    ,
-    setStatus(state, action){
-        state.status = action.payload
-    }
-}
+    //
+    //    setProduct(state, action){state.data = action.payload}
+    //,
+    //setStatus(state, action){
+    //    state.status = action.payload
+    //}
+},
+extraReducers: (builder) => {
+    builder
+        .addCase(fetchProducts.pending, (state, action) => {
+            state.status = STATUS.LOADING;
+        })
+        .addCase(fetchProducts.fulfilled, (state, action) => {
+            state.data = action.payload;
+            state.status = STATUS.IDLE;
+        })
+        .addCase(fetchProducts.rejected, (state, action) => {
+            state.status = STATUS.ERROR;
+        });
+},
 });
 
 export const { setProduct, setStatus} = productSlice.actions;
 export default productSlice.reducer;
 
+export const fetchProducts = createAsyncThunk('products/fetch', async () => {
+    const res = await fetch('https://fakestoreapi.com/products');
+    const data = await res.json();
+    return data;
+});
 
 
-
-export function fetchProducts(){
-    return async function fetchProductThunk(dispatch, getState){
-           dispatch(setStatus(STATUS.LOADING));
-        try{
-            const res = await fetch("https://fakestoreapi.com/products");
-            const data = await res.json();
-             dispatch(setProduct(data));
-             dispatch(setStatus(STATUS.IDLE));
-        }catch(err){
-            console.log(err)
-            dispatch(setStatus(STATUS.ERROR));
-        }
-    }
-}
+//export function fetchProducts(){
+//    return async function fetchProductThunk(dispatch, getState){
+//           dispatch(setStatus(STATUS.LOADING));
+//        try{
+//            const res = await fetch("https://fakestoreapi.com/products");
+//            const data = await res.json();
+//             dispatch(setProduct(data));
+//             dispatch(setStatus(STATUS.IDLE));
+//        }catch(err){
+//            console.log(err)
+//            dispatch(setStatus(STATUS.ERROR));
+//        }
+//    }
+//}
